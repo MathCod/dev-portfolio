8b9e18fb6f70c7994e32638d1308a927de0108ca
Mathias BERGER <mathias.codage@gmail.com>
2026-04-02 21:51:44 +0200

    ➕🎨icons Skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,21 +1,64 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
+import { FaReact, FaNodeJs, FaCss3Alt, FaDatabase } from 'react-icons/fa';
+import { FcApproval, FcBiotech } from "react-icons/fc";
+import { VscCode } from 'react-icons/vsc';
+import { 
+  SiJavascript, 
+  SiTailwindcss, 
+  SiFramer, 
+  SiExpress, 
+  SiMongodb, 
+  SiGit, 
+  SiGithub, 
+  SiFigma, 
+  SiVite, 
+  SiSass,
+  SiPostman, 
+  SiVercel, 
+  SiHtml5,
+  SiReactrouter
+} from 'react-icons/si';
 
 const Skills = () => {
   const categories = [
     {
-      title: "// FRONTEND",
-      items: ["React.js", "JavaScript", "Tailwind CSS", "Sass", "HTML5 / CSS3", "Framer Motion", "React Router", "Responsive Design", "SEO"]
-    },
-    {
-      title: "// BACKEND",
-      items: ["Node.js", "Express", "APIs REST", "MongoDB", "NoSQL"]
-    },
-    {
-      title: "// TOOLS",
-      items: ["Git / GitHub", "VS Code", "Figma", "Agile / Scrum", "Vite", "Postman", "Vercel"]
-    }
-  ];
+    title: "// FRONTEND",
+    items: [
+      { name: "React.js", icon: <FaReact className="text-[#61DAFB]" /> },
+      { name: "JavaScript", icon: <SiJavascript className="text-[#F7DF1E]" /> },
+      { name: "Tailwind CSS", icon: <SiTailwindcss className="text-[#06B6D4]" /> },
+      { name: "Sass", icon: <SiSass className="text-[#a743a9]" /> },
+      { name: "HTML5", icon: <SiHtml5 className="text-[#c2530e]" /> },
+      { name: "CSS3", icon: <FaCss3Alt className="text-[#244cfe]" /> },
+      { name: "Framer Motion", icon: <SiFramer className="text-light" /> },
+      { name: "React Router", icon: <SiReactrouter className="text-[#992201]" /> },
+      { name: "Responsive Design", icon: <FcBiotech /> },
+      { name: "SEO", icon: <FcApproval /> },
+    ]
+  },
+  {
+    title: "// BACKEND",
+    items: [
+      { name: "Node.js", icon: <FaNodeJs className="text-[#339933]" /> },
+      { name: "Express", icon: <SiExpress className="text-light" /> },
+      { name: "MongoDB", icon: <SiMongodb className="text-[#47A248]" /> },
+      { name: "NoSQL", icon: <FaDatabase className="text-[#0060f9]" /> },
+    ]
+  },
+  {
+    title: "// TOOLS",
+    items: [
+      { name: "Git", icon: <SiGit className="text-[#F24E1E]" />},
+      { name: "GitHub", icon: <SiGithub className="text-light" /> },
+      { name: "VS Code", icon: <VscCode className="text-[#007ACC]" /> },
+      { name: "Figma", icon: <SiFigma className="text-[#F24E1E]" /> },
+      { name: "Vite", icon: <SiVite className="text-[#646CFF]" /> },
+      { name: "Postman", icon: <SiPostman className="text-[#FF6C37]" /> },
+      { name: "Vercel", icon: <SiVercel className="text-light" /> },
+    ]
+  }
+];
 
   return (
     <section id="skills" className="py-30 px-6 md:px-20 border-t border-white/5 bg-slate-800">
@@ -42,10 +85,14 @@ const Skills = () => {
               </h3>
               <ul className="space-y-4">
                 {cat.items.map((skill) => (
-                  <li key={skill} className="group flex items-center gap-3">
-                    <div className="w-1.5 h-1.5 bg-neon-purple rounded-full opacity-0 group-hover:opacity-100 transition-opacity"></div>
-                    <span className="text-gray-300 text-2xl group-hover:text-light group-hover:translate-x-2 transition-all duration-300 font-light tracking-wide">
-                      {skill}
+                  <li key={skill.name} className="group flex items-center gap-3">
+                    {/* L'icône avec une petite animation au survol */}
+                    <span className="text-2xl group-hover:scale-120 group-hover:text-neon-purple transition-all duration-300">
+                      {skill.icon}
+                    </span>
+                    
+                    <span className="text-gray-400 group-hover:text-neon-purple group-hover:translate-x-1 transition-all duration-300 font-light tracking-wide text-xl">
+                      {skill.name}
                     </span>
                   </li>
                 ))}
